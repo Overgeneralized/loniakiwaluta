9c1b919bf870d152b95f6ab654ffeec86e854f0c
Overgeneralized <hubert1509j@wp.pl>
2025-03-29 22:07:08 +0100

    Refactor Loniak value calculation and update exchange rate display

--- app.js
+++ app.js
@@ -3,35 +3,50 @@ const polishMonths = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwie
 async function fetchLoniakHistory() {
     let response = await fetch('loniakiHistory.json');
     let jsonData = await response.json();
-    console.log(jsonData);
     return jsonData.history;
 }
 
 function calculateLoniakValue(daysSince, candlestickHeight, notesInCirculation) {
-    let y = candlestickHeight < 0 ? notesInCirculation * 0.1 : 0.05
+    let y = candlestickHeight < 0 ? notesInCirculation * -0.1 : 0.05
     return 1 + (daysSince * 0.05 - candlestickHeight + y) / 2
 }
 
 async function initializeApp() {
     var loniakHistory = await fetchLoniakHistory();
+
     let stockChartData = []
     let valueChartData = []
     let lastCandlestickHeight = 0;
     let currentDate = new Date(2025, 3, 26);
+    let maxCandlestickHeight = 0;
+    let minCandlestickHeight = 0;
     for (let i = 0; i < loniakHistory.length; i++) {
         let currentData = loniakHistory[i];
+        let currentCandlestickHeight = lastCandlestickHeight + currentData.candlestickHeight
+        if (currentCandlestickHeight > maxCandlestickHeight) {
+            maxCandlestickHeight = currentCandlestickHeight
+        }
+        if (currentCandlestickHeight < minCandlestickHeight) {
+            minCandlestickHeight = currentCandlestickHeight
+        }
         let currentWrittenDate = currentDate.getDate() + " " + polishMonths[currentDate.getMonth()]
         let currentLoniakValue = calculateLoniakValue(i, currentData.candlestickHeight, currentData.notesInCirculation);
+        if (i == loniakHistory.length - 1) {
+            exchangeRate = currentLoniakValue
+        }
         valueChartData.push({
             x: currentWrittenDate,
-            y: currentLoniakValue
+            y: currentLoniakValue.toFixed(2)
         })
         stockChartData.push({
             x: currentWrittenDate,
-            y: [lastCandlestickHeight, 0, 0, currentData.candlestickHeight]
+            y: [lastCandlestickHeight.toFixed(2), 1, 1, currentCandlestickHeight.toFixed(2)]
         })
+        lastCandlestickHeight = currentCandlestickHeight;
         currentDate.setDate(currentDate.getDate() + 1);
     }
+
+    document.getElementById("exchange-rate").innerText = exchangeRate.toFixed(2)
 
     var valueChartOptions = {
         chart: {
@@ -39,10 +54,7 @@ async function initializeApp() {
         },
         series: [{
             data: valueChartData
-          }], 
-          xaxis: {
-            type: 'datetime'
-          }  
+        }]
     }
 
     let valueChart = new ApexCharts(document.getElementById("valueChart"), valueChartOptions);
@@ -54,16 +66,20 @@ async function initializeApp() {
         },
         series: [{
             data: stockChartData
-        }]
+        }],
+        yaxis: {
+            max: maxCandlestickHeight + 1,
+            min: minCandlestickHeight - 1
+        }
     }
 
     let stockChart = new ApexCharts(document.getElementById("stockChart"), stockChartOptions);
     stockChart.render()
 }
 
-initializeApp();
+var exchangeRate
 
-let exchangeRate = 1.23;
+initializeApp();
 
 function convertToLoniak() {
     let pln = document.getElementById('plnInput').value;
